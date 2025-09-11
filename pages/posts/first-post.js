01526fd49de94eeeed504333086ce02401d42c93
Matthew <matteidsen@gmail.com>
2025-09-10 21:48:32 -0500

    add basic styles and some customization

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
 import Layout from '../../components/layout';
 
 export default function FirstPost() {
